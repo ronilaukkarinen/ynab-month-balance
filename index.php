9d70be78cfba3dcf779e735961c07c70ab698e15
Roni Laukkarinen <roni@dude.fi>
2022-08-17 00:35:12 +0300

    Improvements

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+// List this month's income
+if ( '2022-08' === date( 'Y-m' ) ) {
+  $incomeItems = [
+    '2411.10', // Palkka
+    '679.20', // Työkkärituki
+    '199.72', // Lapsilisä
+    '416.91', // Vammaistuki
+    '150.00', // Vuokravakuus
+    '1246.44', // Veronpalautus
+  ];
+} else {
+  $incomeItems = [
+    '2411.10', // Palkka
+    '679.20', // Työkkärituki
+    '199.72', // Lapsilisä
+    '416.91', // Vammaistuki
+  ];
+}
+
 // Require composer
 require __DIR__ . '/vendor/autoload.php';
 
@@ -22,20 +41,18 @@ $dotenv->load();
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <style type="text/css">
+:root {
+  --color-red: #ec1b4b;
+  --color-green: #1ccc5c;
+}
+
 body {
   background-color: #0d1117;
   color: #f0f6fc;
   font-family: 'Inter', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', sans-serif;
-}
-
-.value {
-  display: block;
-  font-size: 80px;
-  font-weight: 700;
-  margin-bottom: 10px;
 }
 
 .unit {
@@ -45,18 +62,31 @@ body {
 }
 
 .red {
-  color: #ec1b4b;
+  color: var(--color-red);
 }
 
 .green {
-  color: #1ccc5c;
+  color: var(--color-green);
+}
+
+.value {
+  display: inline-block;
+  font-size: 80px;
+  font-weight: 700;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+
+.pre {
+  display: none;
+  font-size: 14px;
+  line-height: 1.77;
 }
 
 .label {
-  display: block;
-  font-size: 14px;
-  line-height: 1.5;
-  opacity: .5;
+  display: inline-block;
+  font-size: 16px;
+  line-height: 1.77;
 }
 
 .item {
@@ -67,11 +97,18 @@ body {
 
 .powered {
   color: #fff;
-  display: block;
+  display: inline-block;
   text-decoration: none;
   margin-top: 60px;
   opacity: .2;
   transition: all 180ms;
+}
+
+.explanation {
+  color: #898989;
+  font-size: 13px;
+  line-height: 1.77;
+  margin: 15px 0 0 0;
 }
 
 .powered:hover {
@@ -84,6 +121,42 @@ body {
   display: block;
 }
 
+.progress-bar,
+.progress-bar-expenses {
+  border-radius: 4px;
+  height: 22px;
+  white-space: nowrap;
+}
+
+.progress-bar {
+  background-color: rgba(255 255 255 / .2);
+  margin-top: 20px;
+  max-width: 600px;
+  width: 100%;
+}
+
+.progress-bar-expenses {
+  background-color: #85253b;
+  padding: 0 5px;
+  display: flex;
+  align-items: center;
+}
+
+.progress-bar-value {
+  color: #fff;
+  font-weight: 600;
+  display: inline-block;
+  font-size: 13px;
+  margin-right: 10px;
+}
+
+.progress-bar-label {
+  color: #fff;
+  display: inline-block;
+  font-size: 10px;
+  opacity: .5;
+}
+
 @media (max-width: 600px) {
   .item {
     padding: 20px 15px;
@@ -91,6 +164,10 @@ body {
 
   .value {
     font-size: 62px;
+  }
+
+  .label {
+    font-size: 14px;
   }
 }
 </style>
@@ -160,30 +237,14 @@ $response_budgets = json_decode( $get_budgets, true );
 $get_months = callAPI( 'GET', $base . '/budgets/' . $budgetId . '/months/' . date( 'Y-m' ) . '-01', false );
 $response_months = json_decode( $get_months, true );
 
-// List this month's income
-if ( '2022-08' === date( 'Y-m' ) ) {
-  $incomeItems = [
-    '2411.10', // Palkka
-    '679.20', // Työkkärituki
-    '199.72', // Lapsilisä
-    '416.91', // Vammaistuki
-    '1246.44', // Veronpalautus
-  ];
-} else {
-  $incomeItems = [
-    '2411.10', // Palkka
-    '679.20', // Työkkärituki
-    '199.72', // Lapsilisä
-    '416.91', // Vammaistuki
-  ];
-}
 $income = array_sum( $incomeItems );
 
 // Get this month's underfunded amount
 $underfunded = 0;
 $budgeted = 0;
 foreach ( $response_months as $month ) {
-  $budgeted = $month['month']['budgeted'];
+  $budgeted = $month['month']['budgeted'] / 1000;
+  $activity = abs( $month['month']['activity'] / 1000 );
 
   foreach ( $month['month']['categories'] as $category ) {
     $underfunded += $category['goal_under_funded'];
@@ -207,20 +268,37 @@ $underfunded = $underfunded / 1000;
 $transactions = abs( $transactionItems / 1000 );
 
 // Calculate
-$substraction = $income - ( $underfunded + $transactions );
+$substraction = ( $income - $transactions ) - $underfunded;
 ?>
 
 <div class="item">
   <?php
     if ( $substraction > 0 ) {
       $class = 'green';
+      $msg = 'ali varojen';
     } else {
       $class = 'red';
+      $msg = 'yli varojen';
     }
 
-    echo '<span class="value ' . $class . '">' . $substraction . ' <span class="unit">&euro;</span></span>';
+    echo '<span class="pre ' . $class . '">Elät</span><span class="value ' . $class . '">' . abs( $substraction ) . ' <span class="unit">&euro;</span></span>';
   ?>
-  <span class="label">Jäljellä budjetoidusta määrästä tässä kuussa tuloihin nähden</span>
+  <span class="label <?php echo $class; ?>"><?php echo $msg; ?></span>
+
+  <div class="progress-bar" style="display: none;">
+    <div class="progress-bar-expenses" style="width: <?php echo round( ( $budgeted / $income ) * 100, 0 ); ?>%">
+      <span class="progress-bar-value"><?php echo round( $budgeted, 0 ); ?> &euro; / <?php echo round( $income, 0 ); ?> &euro;</span>
+      <span class="progress-bar-label">Menot / Tulot</span>
+    </div>
+  </div>
+
+  <p class="explanation">
+    <span>Tämän kuun tulot on <?php echo round( $income, 0 ); ?> &euro;<br></span>
+    <span style="display: none;">Tälle kuulle budjetoitu <?php echo round( $budgeted, 0 ); ?> &euro;<br></span>
+    <span>Rahaa käytetty <?php echo round( $transactions, 0 ); ?> &euro;<br></span>
+    <span>Tuloista kulujen jälkeen jää vielä <b style="font-weight: 600;" class="green"><?php echo round( $income - $transactions, 0 ); ?> &euro;</b><br></span>
+    <span>Tässä kuussa tarvitaan vielä <b style="font-weight: 600;" class="green"><?php echo round( $underfunded, 0 ); ?> &euro;</b><br></span>
+  </p>
 
   <a class="powered" href="https://app.youneedabudget.com/"><span>Rajapinnan tarjoaa</span><svg aria-label="YNAB" fill="none" height="29" viewBox="0 0 115 29" width="115" xmlns="http://www.w3.org/2000/svg"><path d="M18.249 17.795v10.091H9.766v-10.09L0 0h9.719l4.447 8.527C15.66 5.763 17.143 2.682 18.543 0h9.307zm26.544 10.091l-7.954-13.02v13.02h-8.036V0h7.248l7.953 12.808V0h7.99v27.886zm27.391-4.951h-8.201l-1.647 4.94h-9.06L63.489 0h9.766l9.965 27.886h-9.436zm-4.071-12.761L65.97 16.56h4.283zm27.732 17.712H84.48V0h10.79c6.388 0 10.095 2.717 10.095 7.657 0 2.47-1.283 4.493-3.377 6.01 2.188 1.235 3.541 3.705 3.541 6.222-.011 5.151-3.635 7.997-9.683 7.997zm-1.2-21.382h-2.06v4.528h2.19c1.07 0 2.023-.87 2.023-2.387-.012-1.188-.788-2.14-2.153-2.14zM94.48 16.76h-1.894v4.658h1.777c1.776 0 2.718-.87 2.718-2.27 0-1.482-.742-2.388-2.6-2.388z" fill="#fff"/><path d="M105.776 23.723c0-2.552 2.059-4.61 4.612-4.61S115 21.17 115 23.722s-2.059 4.61-4.612 4.61-4.612-2.058-4.612-4.61z" fill="#fff"/></svg></a>
 </div>
