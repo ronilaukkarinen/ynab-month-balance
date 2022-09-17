09d222e123f9fcfab57a955a86d5d29c5507b5c7
Roni Laukkarinen <roni@dude.fi>
2022-09-17 15:00:18 +0300

    Add alt style, add more parameters, add daily food budget

--- index.php
+++ index.php
@@ -104,9 +104,8 @@ body {
 
 .item {
   max-width: 700px;
-  padding: 20px 50px;
+  padding: 30px;
 }
-
 
 .powered {
   color: #fff;
@@ -170,6 +169,43 @@ body {
   display: none;
   font-size: 10px;
   opacity: .5;
+}
+
+.item-wrapper p {
+  margin: 0;
+}
+
+.item-wrapper-alt {
+  background: rgb(255 255 255 / .08);
+  border-radius: 10px;
+  padding: 21px 30px;
+  margin: 10px 0;
+  width: auto;
+  display: inline-flex;
+}
+
+.item-wrapper-alt:first-of-type {
+  margin-top: 0;
+}
+
+.item-wrapper-alt:last-of-type {
+  margin-bottom: 20px;
+}
+
+.item-wrapper-alt .value {
+  font-size: 52px;
+  margin: 0;
+}
+
+.item-wrapper-alt .unit {
+  font-size: 16px;
+}
+
+.sub-label {
+  font-size: 12px;
+  margin-top: 6px;
+  display: block;
+  opacity: .8;
 }
 
 @media (max-width: 600px) {
@@ -268,15 +304,22 @@ $response_months = json_decode( $get_months, true );
 
 $income = array_sum( $incomeItems );
 
-// Get this month's underfunded amount
+// Get this month's amounts
 $underfunded = 0;
 $budgeted = 0;
+$food_money_available = 0;
+
 foreach ( $response_months as $month ) {
 
   $budgeted = $month['month']['budgeted'] / 1000;
   $activity = abs( $month['month']['activity'] / 1000 );
 
   foreach ( $month['month']['categories'] as $category ) {
+
+    if ( 'f6824431-03d1-4230-80de-126b66bac5d2' === $category['id'] ) {
+      $food_money_available += $category['balance'] / 1000;
+    }
+
     $underfunded += $category['goal_under_funded'];
   }
 }
@@ -311,34 +354,44 @@ $substraction = ( $income - $transactions ) - $underfunded;
 ?>
 
 <div class="item">
-  <?php
-    $msg = 'jää säästöön';
-    if ( $substraction > 0 ) {
-      $class = 'green';
-      $substraction = '+' . abs( $substraction );
-    } else {
-      $class = 'red';
-      $substraction = $substraction;
-    }
+  <div class="item-wrapper item-wrapper-alt">
+    <p>
+      <?php
+        if ( $substraction > 0 ) {
+          $class = 'green';
+          $substraction = '+' . abs( $substraction );
+        } else {
+          $class = 'red';
+          $substraction = $substraction;
+        }
 
-    $balance = number_format( (float) $substraction, 2, ',', '' );
-    echo '<span class="value ' . $class . '">' . $balance . ' <span class="unit">&euro;</span></span>';
-  ?>
-  <span class="label <?php echo $class; ?>" style="display: none;"><?php echo $msg; ?></span>
+        // Print balance
+        $balance = number_format( (float) $substraction, 2, ',', '' );
+        echo '<span class="value ' . $class . '">' . $balance . ' <span class="unit">&euro;</span></span>';
 
-  <div class="progress-bar">
-    <div class="progress-bar-expenses background-<?php echo $class; ?>" style="width: <?php echo round( ( $budgeted / $income ) * 100, 0 ); ?>%">
-      <span class="progress-bar-value"><?php echo round( $budgeted, 0 ); ?> &euro; / <?php echo round( $income, 0 ); ?> &euro;</span>
-      <span class="progress-bar-label">Menot / Tulot</span>
-    </div>
+        // Calculate days remaining this month
+        $timestamp = strtotime( 'now' );
+        $days_remaining_this_month = (int) date( 't', $timestamp ) - (int) date( 'j', $timestamp );
+      ?>
+      <span class="sub-label <?php echo $class; ?>">Rahatilanne nyt</span></span>
+    </p>
+  </div><br>
+
+  <div class="item-wrapper item-wrapper-alt">
+    <p>
+      <span class="value green"><?php echo number_format( (float) $food_money_available / $days_remaining_this_month, 2, ',', '' ); ?> <span class="unit">&euro;</span></span><br />
+      <span class="sub-label green">Reaaliaikainen päiväbudjetti</span></span>
+    </p>
   </div>
 
   <p class="explanation">
-    <span>Tämän kuun tulot on <b style="font-weight: 500;" class="neutral"><?php echo round( $income, 2 ); ?> &euro;</b><br></span>
-    <span style="display: none;">Tälle kuulle budjetoitu <b style="font-weight: 500;" class="neutral"><?php echo round( $budgeted, 2 ); ?> &euro;</b><br></span>
-    <span>Rahaa käytetty <b style="font-weight: 500;" class="neutral"><?php echo round( $transactions, 2 ); ?> &euro;</b><br></span>
-    <span>Tuloista kulujen jälkeen jää vielä <b style="font-weight: 500;" class="green"><?php echo round( $income - $transactions, 2 ); ?> &euro;</b><br></span>
-    <span>Tässä kuussa tarvitaan vielä <b style="font-weight: 500;" class="green"><?php echo round( $underfunded, 2 ); ?> &euro;</b><br></span>
+    <span>Tämän kuun tulot on <b style="font-weight: 500;" class="neutral"><?php echo number_format( (float) $income, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span>Tämän kuun menot on <b style="font-weight: 500;" class="neutral"><?php echo number_format( (float) $transactions + $underfunded, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span>Ruokabudjetti loppukuulle <?php echo $days_remaining_this_month; ?> päivälle <b style="font-weight: 500;" class="neutral"><?php echo number_format( (float) $food_money_available, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span style="display: none;">Tälle kuulle budjetoitu <b style="font-weight: 500;" class="neutral"><?php echo number_format( (float) $budgeted, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span>Rahaa käytetty tähän mennessä <b style="font-weight: 500;" class="neutral"><?php echo number_format( (float) $transactions, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span>Tuloista kulujen jälkeen jää vielä <b style="font-weight: 500;" class="green"><?php echo number_format( (float) $income - $transactions, 2, ',', '' ); ?> &euro;</b><br></span>
+    <span>Tässä kuussa tarvitaan vielä <b style="font-weight: 500;" class="green"><?php echo number_format( (float) $underfunded, 2, ',', '' ); ?> &euro;</b><br></span>
   </p>
 
   <a class="powered" href="https://app.youneedabudget.com/"><span>Rajapinnan tarjoaa</span><svg aria-label="YNAB" fill="none" height="29" viewBox="0 0 115 29" width="115" xmlns="http://www.w3.org/2000/svg"><path d="M18.249 17.795v10.091H9.766v-10.09L0 0h9.719l4.447 8.527C15.66 5.763 17.143 2.682 18.543 0h9.307zm26.544 10.091l-7.954-13.02v13.02h-8.036V0h7.248l7.953 12.808V0h7.99v27.886zm27.391-4.951h-8.201l-1.647 4.94h-9.06L63.489 0h9.766l9.965 27.886h-9.436zm-4.071-12.761L65.97 16.56h4.283zm27.732 17.712H84.48V0h10.79c6.388 0 10.095 2.717 10.095 7.657 0 2.47-1.283 4.493-3.377 6.01 2.188 1.235 3.541 3.705 3.541 6.222-.011 5.151-3.635 7.997-9.683 7.997zm-1.2-21.382h-2.06v4.528h2.19c1.07 0 2.023-.87 2.023-2.387-.012-1.188-.788-2.14-2.153-2.14zM94.48 16.76h-1.894v4.658h1.777c1.776 0 2.718-.87 2.718-2.27 0-1.482-.742-2.388-2.6-2.388z" fill="#fff"/><path d="M105.776 23.723c0-2.552 2.059-4.61 4.612-4.61S115 21.17 115 23.722s-2.059 4.61-4.612 4.61-4.612-2.058-4.612-4.61z" fill="#fff"/></svg></a>
